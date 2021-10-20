c315c8d23426a1b436e6fdc8b939f727f6f13727
Evan Rispoli <evan.rispoli@gmail.com>
2021-10-20 11:02:42 -0300

    Array e dicionario

---
+++ Programação para internet com JavaScript/main.js
@@ -0,0 +1,34 @@
+var nome = "Evan Rispoli";
+var idade = 37;
+//alert(nome + " tem" + idade + " anos")
+var idade2 = 10;
+var frase = "I live in Canada, my heart is canadian";
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("canadian", "brazilian"));
+//console.log(frase.toLocaleUpperCase());
+//console.log(frase.toLowerCase());
+/*
+var list =["audi", "Buick", "Chrysler" ]
+list.push("BMW");
+list.pop();
+console.log(list);
+console.log(list.toString());
+console.log(list.join(" | "))
+
+var car = {model: "Terrain", color:"Black", year: "2019"}
+console.log(car)
+*/
+var cars = [{
+        model: "Terrain",
+        color: "Black",
+        year: "2019"
+    },
+    {
+        model: "Cherokee",
+        color: "White",
+        year: "2021"
+    }
+]
+alert(cars[0].model)
